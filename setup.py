7af949593010ab4afaed9c0df07a6f1a6555c1ac
geb <853934146@qq.com>
2023-04-17 10:51:54 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="cmip",
-    version="0.0.2",
+    version="0.0.5",
     author="geb",
     author_email="853934146@qq.com",
     description="An efficient information processing program.",
